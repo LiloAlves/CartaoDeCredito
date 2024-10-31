10f13bda1ce542f760210dac45cd965fc33038e6
Lívia Souza <lilosouzaa@gmail.com>
2024-10-31 19:57:50 -0300

    Update CartaoDeCredito.java

--- CartaoDeCredito.java
+++ CartaoDeCredito.java
@@ -4,6 +4,21 @@ public class CartaoDeCredito {
     private String cpf;
     private double limite;
     private double totalFatura;
+    private double taxaCashBack;
+
+public CartaoDeCredito(int numero, String nomeTitular) {
+    this.numero = numero;
+    this.nomeTitular = nomeTitular;
+    this. limite = 1000;
+    this.taxaCashBack = 0.0;
+}
+
+public CartaoDeCredito(int numero, String nomeTitular, double limite, double taxacashBack) {
+    this.numero = numero;
+    this.nomeTitular = nomeTitular;
+    this.limite = limite;
+    this.taxaCashBack = taxaCashBack;
+}
 
        public int getNumero() {
         return this.numero;
@@ -45,6 +60,14 @@ public class CartaoDeCredito {
         this.totalFatura = totalFatura;
     }
 
+    public double getTaxaCashBack() {
+        return this.taxaCashBack;
+    }
+
+    public void setTaxaCashBack(double taxaCashBack) {
+        this.taxaCashBack = taxaCashBack;
+    }
+
     public void consultarLimite() {
         System.out.println("Limite disponível: " + getLimite());
     }
@@ -55,16 +78,29 @@ public class CartaoDeCredito {
 
     public void realizarCompra(double valor) {
         if (valor <= this.limite) {
-            double limiteAnterior = this.getLimite();
-            this.setLimite (limiteAnterior - valor);
-            double total = this.getTotalFatura();
-            this.setTotalFatura (total + valor);
+            this.limite -= valor;
+            this.totalFatura += valor;
+            System.out.println("Compra realizada com sucesso!");
         } 
         
         else {
             System.out.println("Você não possui limite necessario para essa compra");
         }
         
+    }
+
+    public void realizarCompra(double valor, boolean cashBack) {
+        if (valor <= this.limite) {
+            this.limite -= valor;
+            this.totalFatura += valor;
+            double cashback = valor * taxaCashBack;
+            System.out.println("Compra realizada com sucesso! O valor do seu cashBack foi de: " +cashback);
+        } 
+        
+        else {
+            System.out.println("Você não possui limite necessario para essa compra");
+        }
+
     }
 
     public void alterarLimite(double novoLimite) {
