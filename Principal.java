7441c737129be632ff49ce193562f5f6a1ae23dc
Lívia Souza <lilosouzaa@gmail.com>
2024-10-31 19:59:07 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-        CartaoDeCredito c = new CartaoDeCredito();
-        c.setCpf("12312352636");
-        c.setLimite(100);
         Scanner sc = new Scanner(System.in);
+
+        CartaoDeCredito c = new CartaoDeCredito(123321, "Luciano Bastos");
+        c.setCpf("12345543210");
+
+        c.alterarLimite(1000);
+        c.setTaxaCashBack(0.05);
 
         int opcao = 1;
 
@@ -13,6 +16,7 @@ public class Principal {
             System.out.println("1 - Consultar Limite");
             System.out.println("2 - Consultar Fatura");
             System.out.println("3 - Realizar compra");
+            System.out.println("4 - Realizar compra com o cashback");
             System.out.println("0 - Sair");
 
             opcao = sc.nextInt();
@@ -28,6 +32,11 @@ public class Principal {
                     System.out.println("Digite o valor da compra:");
                     double valor = sc.nextDouble();
                     c.realizarCompra(valor);
+                    break;
+                case 4: 
+                    System.out.println("Digite o valor da compra:");
+                    double cashBack = sc.nextDouble();
+                    c.realizarCompra(cashBack, true);
                     break;
                 case 0:
                     System.out.println("Saindo...");
